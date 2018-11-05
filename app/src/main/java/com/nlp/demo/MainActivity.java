36ea5566c79c021ac87060ce9c45629004a861a9
zdd <zdd2389@gmail.com>
2018-11-05 17:43:36 +0800

    测试

--- app/src/main/java/com/nlp/demo/MainActivity.java
+++ app/src/main/java/com/nlp/demo/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnScan=(Button)findViewById(R.id.btn_scan);
 
+
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
